eb7ac1dcd0abf7b1039e176f9537a24b57a84283
Aru <rm.arumugam.2000@gmail.com>
2022-02-11 23:14:16 +0800

    Added some configs

--- src/envs/mpe_spread_env.py
+++ src/envs/mpe_spread_env.py
@@ -5,9 +5,9 @@ import numpy as np
 
 class SimpleSpreadEnv(MultiAgentEnv):
 
-    def __init__(self, N=3, episode_limit=25, **kwargs) -> None:
+    def __init__(self, N=3, episode_limit=25, local_ratio=0.5, **kwargs) -> None:
         super().__init__()
-        env = simple_spread_v2.parallel_env(N=N, max_cycles=episode_limit)
+        env = simple_spread_v2.parallel_env(N=N, max_cycles=episode_limit, local_ratio=local_ratio)
         self.env = ss.pettingzoo_env_to_vec_env_v0(env)
         self.observation = self.env.reset()
         self.n_actions = 5
