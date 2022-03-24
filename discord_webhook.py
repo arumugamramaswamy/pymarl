b344c43a94e10de3b9b867a0c477cb0bc4aa344f
Aru <rm.arumugam.2000@gmail.com>
2022-03-24 14:07:52 +0800

    Added discord webhook

---
+++ discord_webhook.py
@@ -0,0 +1,32 @@
+import requests
+import sys
+
+if len(sys.argv) != 2:
+    print("Usage: python discord_webhook.py MESSAGE")
+    print()
+    print("Missing argument MESSAGE")
+    sys.exit(-1)
+
+with open("webhook-url") as f:
+    url = f.read()
+
+#for all params, see https://discordapp.com/developers/docs/resources/webhook#execute-webhook
+data = {}
+
+#for all params, see https://discordapp.com/developers/docs/resources/channel#embed-object
+data["embeds"] = [
+    {
+        "description" : f"{sys.argv[1]}",
+        "title" : "Experiment Completed"
+    }
+]
+
+result = requests.post(url, json = data)
+
+try:
+    result.raise_for_status()
+except requests.exceptions.HTTPError as err:
+    print(err)
+else:
+    print("Payload delivered successfully, code {}.".format(result.status_code))
+
